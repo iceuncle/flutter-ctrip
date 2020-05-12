3bf607576018c226202703d86cf6bbe3231ef525
wutianyang <1582869895@qq.com>
2020-05-12 21:26:23 +0800

    完成旅拍和我的模块，实现滑动tab及瀑布流效果

--- lib/pages/travel_page.dart
+++ lib/pages/travel_page.dart
@@ -1,16 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ctrip/dao/travel_tab_dao.dart';
+import 'package:flutter_ctrip/model/travel_tab_model.dart';
+import 'package:flutter_ctrip/pages/travel_tab_page.dart';
 
 class TravelPage extends StatefulWidget {
   @override
   _TravelPageState createState() => _TravelPageState();
 }
 
-class _TravelPageState extends State<TravelPage> {
+class _TravelPageState extends State<TravelPage> with TickerProviderStateMixin {
+  TabController _controller;
+  List<TravelTab> tabs = [];
+  TravelTabModel travelTabModel;
+
+  @override
+  void initState() {
+    _controller = TabController(length: 0, vsync: this);
+    fetchData();
+    super.initState();
+  }
+
+  fetchData() {
+    TravelTabDao.fetch().then((TravelTabModel model) {
+      print(model);
+      _controller = TabController(
+          length: model.tabs.length, vsync: this); //fix tab label 空白问题
+      setState(() {
+        tabs = model.tabs;
+        travelTabModel = model;
+      });
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('旅拍'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.only(top: 30),
+            child: TabBar(
+                controller: _controller,
+                isScrollable: true,
+                labelColor: Colors.black,
+                labelPadding: EdgeInsets.fromLTRB(20, 0, 10, 5),
+                indicator: UnderlineTabIndicator(
+                    borderSide: BorderSide(color: Color(0xff2fcfbb), width: 3),
+                    insets: EdgeInsets.only(bottom: 10)),
+                tabs: tabs.map<Tab>((TravelTab tab) {
+                  return Tab(text: tab.labelName);
+                }).toList()),
+          ),
+          Flexible(
+              child: TabBarView(
+                  controller: _controller,
+                  children: tabs.map((TravelTab tab) {
+                    return TravelTabPage(
+                      travelUrl: travelTabModel.url,
+                      groupChannelCode: tab.groupChannelCode,
+                    );
+                  }).toList()))
+        ],
       ),
     );
   }
